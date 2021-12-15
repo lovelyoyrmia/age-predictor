74f5d8961a870fe00988cf230b9ccb512a639766
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-15 21:04:58 +0700

    add load image path

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
+import os
+
 import streamlit as st
 import ktrain
 import cv2
+import io
 from PIL import Image
 
 model = ktrain.load_predictor("models/")
 
 
-def real_prediction(fname):
+def real_prediction(fname, model):
     pred = round(model.predict_filename(fname)[0])
-    image = cv2.imread(fname)
-    cv2.imshow("", image)
-    print("Predicted age: %s" % pred)
-    cv2.waitKey(0)
+    return pred
 
 
 st.title("Age Detection")
@@ -23,8 +23,23 @@ def load_image(image):
     return image_load
 
 
+def load_imgpath(image_path):
+    try:
+        with open(os.path.join("uploads", image_path.name), "wb") as f:
+            f.write(image_path.getbuffer())
+        return 1
+    except:
+        return 0
+
+
 image_upload = st.file_uploader("Pick Your Image", type=["jpg", "jpeg", "png"])
 
-if image_upload:
-    image = load_image(image_upload)
-    st.image(image)
+if image_upload is not None:
+    if load_image(image_upload):
+        image = load_image(image_upload)
+        st.image(image)
+        # buf = io.BytesIO()
+        # image.save(buf, "JPEG")
+        # img_bytes = buf.getvalue()
+        pred = real_prediction(os.path.join("uploads", image_upload.name), model)
+        st.subheader(f"Predicted age {pred} year old")
