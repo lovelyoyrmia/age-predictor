fdf7836046326db857338b58232c385546c0ae90
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-15 21:19:49 +0700

    load image path from uploads folder

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import os
 
 import streamlit as st
 import ktrain
-import cv2
-import io
 from PIL import Image
 
 model = ktrain.load_predictor("models/")
@@ -35,11 +33,8 @@ def load_imgpath(image_path):
 image_upload = st.file_uploader("Pick Your Image", type=["jpg", "jpeg", "png"])
 
 if image_upload is not None:
-    if load_image(image_upload):
+    if load_imgpath(image_upload):
         image = load_image(image_upload)
         st.image(image)
-        # buf = io.BytesIO()
-        # image.save(buf, "JPEG")
-        # img_bytes = buf.getvalue()
         pred = real_prediction(os.path.join("uploads", image_upload.name), model)
         st.subheader(f"Predicted age {pred} year old")
