d4b9d00db6d522665b74bad3e060e57456c0efb8
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-15 21:05:40 +0700

    see shape of image

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
 import ktrain
 import cv2
+import numpy as np
+import PIL.Image as Image
 
-model = ktrain.load_predictor('models/')
+model = ktrain.load_predictor("models/")
+
 
 def real_prediction(fname):
-  pred = round(model.predict_filename(fname)[0])
-  image = cv2.imread(fname)
-  cv2.imshow('',image)
-  print('Predicted age: %s' % pred)
-  cv2.waitKey(0)
+    files = Image.open(fname)
+    image_array = np.array(files.convert("RGB"))
+    # pred = model.predict(image_array)
+    # image = cv2.imread(files)
+    # cv2.imshow("", files)
+    # print("Predicted age: %s" % pred)
+    # cv2.waitKey(0)
+    print(image_array.shape)
 
-real_prediction('IMG-20190916-WA0000-1.jpg')
+
+real_prediction("IMG-20190916-WA0000-1.jpg")
